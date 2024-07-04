329de7863e7a48fe94879ad5fd1353c2e88ef70c
Namdroidd <roselynecuka@gmai.com>
2024-07-04 10:18:39 +0300

    Add RAG

--- src/main.py
+++ src/main.py
@@ -6,68 +6,94 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-import google.generativeai
-import os
+import requests
+import bs4
 
 # Load environment variables
 load_dotenv()
 
-genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+google_api_key = os.getenv("GOOGLE_API_KEY")
+if not google_api_key:
+    raise EnvironmentError("GOOGLE_API_KEY environment variable not set. Please set it for Google Generative AI.")
 
-# Load document
-loader = WebBaseLoader(["https://kenyanwallstreet.com/safaricom-and-pezesha-introduce-mkopo-wa-pochi-to-lend-small-businesses/"])
-data = loader.load()
-# print(data)
+# Configure Google API key for generative AI
+genai.configure(api_key=google_api_key)
 
-# Extract text content from the loaded data
-content = "\n\n".join(doc.page_content for doc in data)
+# Define model names
+Generative_model = 'gemini-1.5-pro'
+embedding_model_name = "models/embedding-001"
+embedding_model = GoogleGenerativeAIEmbeddings(model=embedding_model_name, task_type="retrieval_document")
 
-# Split text into chunks
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, 
-    chunk_overlap=100,
-    separators=["\n\n", "\n", " ", ""]
+# Initialize the generative AI model for conversation
+llm = ChatGoogleGenerativeAI(model=Generative_model, temperature=0.7)
+
+
+
+# Define the URL to load
+urls = ["https://kenyanwallstreet.com/safaricom-and-pezesha-introduce-mkopo-wa-pochi-to-lend-small-businesses/",
+        "https://kenyanwallstreet.com/nses-next-listing-expected-to-be-marula-mining/", "https://kenyanwallstreet.com/value-of-public-assets-traced-by-eacc-declines-to-ksh-6-6bn-in-2022-23/",
+        "https://kenyanwallstreet.com/sgr-passenger-traffic-drops-by-11-per-cent-in-q1-2024/S",
+        "https://www.kenyamoja.com/business/kenyan-wallstreet"]
+
+# Initialize WebBaseLoader with BeautifulSoup parsing options
+loader = WebBaseLoader(
+    web_paths=urls,
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            ("body")
+        )
+    ),
 )
-text_chunks = text_splitter.split_text(content)
-print(text_chunks)
-# print(content)
 
-# text = text_splitter.split_text(content)
+# Load text from URLs
+documents = loader.load()
 
-# print(text[700])
+# Extract text content from documents
+split_text = [doc.page_content for doc in documents]
 
-# # Create embeddings for text chunks
+# Initialize the vector database with the embedding model
+vectordb = Chroma(embedding_function=embedding_model)
 
-# embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-# vectore_store = Chroma.from_texts(text, embeddings).as_retriever()
+# Add documents to the vector database
+vectordb.add_texts(texts=split_text)
 
-# # Create prompt
+# print("Documents have been successfully added to the vector database.")
 
-# prompt_template = """" 
-# You are an AI assistant with access to a document containing information about using Django REST 
-# Framework 
-# (DRF) in the context of IoT. Your task is to provide relevant information based on this document. 
-# If the document does not contain the relevant information, provide a generic answer and prompt the 
-# user for more specific content.
-# \n\n
+retriever = vectordb.as_retriever()
 
-# context :\n {context}?\n
-# question :\n {question}?\n
+print(retriever)
 
-# Answer:
+# Create prompt template for RAG
+prompt_template = PromptTemplate(
+    input_variables=["context", "question"],
+    template="""
+    You are an AI assistant with access to a document containing information about investing in Kenya. 
+    Your task is to provide relevant information based on this document. If the document does not contain 
+    the relevant information, provide a generic answer and prompt the user for more specific content.
 
-# """
+    Context:
+    {context}
 
-# prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    Question:
+    {question}
 
-# model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
-# chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    Answer:
+    """
+)
 
-# question = input("Enter your question: ")
-# docs = vectore_store.invoke({"query": question})
 
-# response = chain.invoke({"input_documents":docs, "question":question},
-#                  return_only_outputs=True)
-# print(response)
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt_template
+    | llm
+    | StrOutputParser()
+)
+
+rag_chain.invoke("What are investments?")
 
